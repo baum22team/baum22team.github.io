87e178450d8db51e78fad8625ea81aacd3380bbd
lefa22 <66741521+lefa22@users.noreply.github.com>
2023-07-08 17:34:04 +0200

    added animation and menu content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
+var toggleON = false;
+
 function toggleMenu() {
     var menu = document.getElementById("menuContainer");
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
+    var menuButton = document.getElementById("menu-button")
+    if (toggleON) {
+      menuButton.classList = "fa fa-bars"
+      toggleON = false;
+      setTimeout(()=>{document.getElementById("menuContainer").style.display = "none"}, 505)
     } else {
-      menu.style.display = "block";
+      document.getElementById("menuContainer").style.display = ""
+      menuButton.classList = "fa fa-xmark"
+      toggleON = true;
     }
+
+    menu.classList.toggle("displayNone")
+    menu.classList.toggle("displayBlock")
   }
+
+
+
+setTimeout(()=>{document.body.classList.remove("preload")}, 600)
