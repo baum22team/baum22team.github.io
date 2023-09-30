095a94af4ea37a4a6d78c50af62c0da1a6bdb03e
lefa22 <lefa22@xyz.de>
2023-09-30 22:18:53 +0200

    hotfix header + footer

--- mainAssets/main.js
+++ mainAssets/main.js
@@ -1,4 +1,4 @@
 $(function(){
-    $("header").load("https://baum22team.githun.io/mainAssets/header.html"); 
-    $("footer").load("https://baum22team.githun.io/mainAssets/footer.html"); 
+    $("header").load("https://baum22team.github.io/mainAssets/header.html"); 
+    $("footer").load("https://baum22team.github.io/mainAssets/footer.html"); 
 });
